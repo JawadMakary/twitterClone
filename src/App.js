aa977f3a04bd20fd7d69cdad0b828904bd7cc998
jawad <jawadhalbymakary@gmail.com>
2021-07-26 01:15:22 +0300

    fix sidebar design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Feed from './components/Feed';
 import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="app">
     <Sidebar />
+    <Feed />
     </div>
   );
 }
